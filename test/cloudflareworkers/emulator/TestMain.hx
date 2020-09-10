861d8b64647a68c14cea7adeaa6ed51894d70d7a
s_tomikawa <s_tomikawa@denkiyagi.jp>
2020-09-10 17:02:30 +0900

    Fix indent spaces 2 to 4

--- test/cloudflareworkers/emulator/TestMain.hx
+++ test/cloudflareworkers/emulator/TestMain.hx
@@ -3,7 +3,7 @@ package cloudflareworkers.emulator;
 import buddy.Buddy;
 
 class TestMain implements Buddy<[
-  cloudflareworkers.emulator.BodyTest,
-  cloudflareworkers.emulator.RequestTest,
-  cloudflareworkers.emulator.ResponseTest
+    cloudflareworkers.emulator.BodyTest,
+    cloudflareworkers.emulator.RequestTest,
+    cloudflareworkers.emulator.ResponseTest
 ]> {}
