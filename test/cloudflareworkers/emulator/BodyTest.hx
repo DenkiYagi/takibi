6c22c60e997f1a3b52cca038b7a51e53ae6c69fb
s_tomikawa <s_tomikawa@denkiyagi.jp>
2020-09-28 17:54:30 +0900

    URLSearchParamsとReadableStreamのテストケースを追加

--- test/cloudflareworkers/emulator/BodyTest.hx
+++ test/cloudflareworkers/emulator/BodyTest.hx
@@ -1,5 +1,6 @@
 package cloudflareworkers.emulator;
 
+import js.node.url.URLSearchParams;
 import js.html.TextDecoder;
 import js.html.TextEncoder;
 import buddy.BuddySuite;
@@ -7,16 +8,31 @@ using buddy.Should;
 using cloudflareworkers.emulator.Body;
 using cloudflareworkers.emulator.Response;
 using cloudflareworkers.emulator.FormData;
+import js.npm.webstreams_polyfill.ReadableByteStreamController;
+import js.npm.webstreams_polyfill.ReadableStream in WebReadableStream;
+import js.npm.webstreams_polyfill.ReadableStream.UnderlyingByteSourceType;
 import js.lib.Promise;
-import js.lib.ArrayBuffer;
 
 class BodyTest extends BuddySuite {
     public function new() {
         describe("Body.arrayBuffer()", {
             it("should return Promise has ArrayBuffer of expected string", done -> {
+                final urlSearchParams = new URLSearchParams({
+                    'key': 'value',
+                    'symbols': '~`!@#$%^&*()_+-={}|[]\\;\':"/.,?>< '
+                });
+                final readableStream = new ReadableStream(new WebReadableStream({
+                    type: Bytes,
+                    start: (controller: ReadableByteStreamController) -> {
+                        controller.enqueue(new TextEncoder().encode("ReadableStream"));
+                        controller.close();
+                    }
+                }));
                 final testCases:Array<Dynamic> = [
                     { body: "USVString", expect: "USVString" },
                     { body: new TextEncoder().encode("ArrayBuffer"), expect: "ArrayBuffer" },
+                    { body: urlSearchParams, expect: "key=value&symbols=%7E%60%21%40%23%24%25%5E%26*%28%29_%2B-%3D%7B%7D%7C%5B%5D%5C%3B%27%3A%22%2F.%2C%3F%3E%3C+" },
+                    { body: readableStream, expect: "ReadableStream" },
                     { body: null, expect: "" },
                 ];
 
